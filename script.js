8a45c7a9c71b1e137f70cc24751b0c767e22a323
sasa-gamer47 <75442245+sasa-gamer47@users.noreply.github.com>
2021-03-31 13:49:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+alert("Every content in this web page are only a test, for this I'm NOT RESPONSABLE FOR EVERY COPYRIGHT VOLATION!!!!")
+
 const detailButton = document.getElementById('detail-button')
 
 detailButton.addEventListener('click', () => {
